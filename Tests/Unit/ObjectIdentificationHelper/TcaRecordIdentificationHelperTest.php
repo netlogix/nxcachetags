0349dde9ebbf442397ad77dd77740b4d59f59fa9
Alexander Jahn <alexander.jahn@netlogix.de>
2022-07-12 10:09:57 +0200

    PATCH: add test for TcaRecordIdentificationHelper

---
+++ Tests/Unit/ObjectIdentificationHelper/TcaRecordIdentificationHelperTest.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netlogix\Nxcachetags\Tests\Unit\ObjectIdentificationHelper;
+
+use Netlogix\Nxcachetags\ObjectIdentificationHelper\TcaRecordIdentificationHelper;
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+
+class TcaRecordIdentificationHelperTest extends UnitTestCase
+{
+
+    /**
+     * @test
+     * @dataProvider tableNameDataProvider
+     *
+     * @param string $tableName
+     * @return void
+     */
+    public function itCanIdentifyObjectsWithUnderscore(string $tableName)
+    {
+        $uid = rand(1, 99999);
+
+        $subject = new TcaRecordIdentificationHelper();
+
+        $res = $subject->identifyCacheTagForObject(sprintf('%s_%d', $tableName, $uid));
+
+        self::assertCount(1, $res);
+        self::assertEquals(sprintf('%s_%d', $tableName, $uid), $res[0]);
+    }
+
+    /**
+     * @test
+     * @dataProvider tableNameDataProvider
+     *
+     * @param string $tableName
+     * @return void
+     */
+    public function itCanIdentifyObjectsWithDash(string $tableName)
+    {
+        $uid = rand(1, 99999);
+
+        $subject = new TcaRecordIdentificationHelper();
+
+        $res = $subject->identifyCacheTagForObject(sprintf('%s-%d', $tableName, $uid));
+
+        self::assertCount(1, $res);
+        self::assertEquals(sprintf('%s_%d', $tableName, $uid), $res[0]);
+    }
+
+    /**
+     * @test
+     * @dataProvider tableNameDataProvider
+     *
+     * @param string $tableName
+     * @return void
+     */
+    public function itCanIdentifyObjectsWithColon(string $tableName)
+    {
+        $uid = rand(1, 99999);
+
+        $subject = new TcaRecordIdentificationHelper();
+
+        $res = $subject->identifyCacheTagForObject(sprintf('%s:%d', $tableName, $uid));
+
+        self::assertCount(1, $res);
+        self::assertEquals(sprintf('%s_%d', $tableName, $uid), $res[0]);
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function itReturnsEmptyTagsForNonStringInput()
+    {
+        $subject = new TcaRecordIdentificationHelper();
+
+        self::assertEmpty($subject->identifyCacheTagForObject(1));
+        self::assertEmpty($subject->identifyCacheTagForObject(new \stdClass()));
+        self::assertEmpty($subject->identifyCacheTagForObject([]));
+        self::assertEmpty($subject->identifyCacheTagForObject(false));
+    }
+
+    /**
+     * @test
+     *
+     * @return void
+     */
+    public function itReturnsEmptyTagsForUnknownRecordIdentifier()
+    {
+        $subject = new TcaRecordIdentificationHelper();
+
+        self::assertEmpty($subject->identifyCacheTagForObject(uniqid()));
+    }
+
+    public function tableNameDataProvider(): array
+    {
+        // no TCA is available in UnitTest
+        $tables = ['pages', 'tt_content', 'be_users', 'sys_file', 'sys_file_reference', 'sys_category', uniqid('tx_')];
+
+        $data = [];
+
+        foreach ($tables as $table) {
+            $data[$table] = [$table];
+        }
+
+        return $data;
+    }
+
+}
