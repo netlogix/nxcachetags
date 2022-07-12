2eef0c731346cb59842b18fbc83cdc91680b387d
Alexander Jahn <alexander.jahn@netlogix.de>
2022-07-12 10:11:26 +0200

    CLEANUP: remove unused injects from BackendSlot

--- Classes/Persistence/Generic/Storage/BackendSlot.php
+++ Classes/Persistence/Generic/Storage/BackendSlot.php
@@ -7,8 +7,6 @@ namespace Netlogix\Nxcachetags\Persistence\Generic\Storage;
 use Netlogix\Nxcachetags\Service\CacheTagService;
 use TYPO3\CMS\Core\SingletonInterface;
 use TYPO3\CMS\Extbase\DomainObject\AbstractDomainObject;
-use TYPO3\CMS\Extbase\Object\ObjectManagerInterface;
-use TYPO3\CMS\Extbase\Persistence\Generic\Mapper\DataMapper;
 
 /**
  * This slot for the Typo3DbBackend flushes cache tags whenever Extbase writes database records.
@@ -17,35 +15,13 @@ class BackendSlot implements SingletonInterface
 {
 
     /**
-     * @var ObjectManagerInterface
-     */
-    protected ObjectManagerInterface $objectManager;
-
-    /**
      * @var CacheTagService
      */
     protected CacheTagService $cacheTagService;
 
-    /**
-     * @var DataMapper
-     */
-    protected DataMapper $dataMapper;
-
-    protected array $newObjectTags = [];
-
-    public function injectObjectManager(ObjectManagerInterface $objectManager)
-    {
-        $this->objectManager = $objectManager;
-    }
-
     public function injectCacheTagService(CacheTagService $cacheTagService)
     {
         $this->cacheTagService = $cacheTagService;
-    }
-
-    public function injectDataMapper(DataMapper $dataMapper)
-    {
-        $this->dataMapper = $dataMapper;
     }
 
     public function flushCacheForObject(AbstractDomainObject $object)
