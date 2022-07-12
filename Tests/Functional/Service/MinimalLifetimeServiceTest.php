7afcfcf1ef8b0f8a2c829ddc5d1b8e58983950cf
Alexander Jahn <alexander.jahn@netlogix.de>
2022-07-12 16:08:54 +0200

    PATCH: add more tests for services

---
+++ Tests/Functional/Service/MinimalLifetimeServiceTest.php
@@ -0,0 +1,111 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netlogix\Nxcachetags\Tests\Functional\Service;
+
+use Netlogix\Nxcachetags\Service\MinimalLifetimeService;
+use Nimut\TestingFramework\TestCase\FunctionalTestCase;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+
+class MinimalLifetimeServiceTest extends FunctionalTestCase
+{
+    /**
+     * @test
+     * @return void
+     */
+    public function itReturnsLifetimeIfNoRecordIsFound() {
+
+        $expires = time();
+
+        $subject = $this->getAccessibleMock(MinimalLifetimeService::class, ['dummy']);
+        $res = $subject->_call('findMinimalLifetimeForRecord', $expires, 'pages', rand(1,9999));
+
+        self::assertEquals($expires, $res);
+    }
+    /**
+     * @test
+     * @dataProvider lifetimeDataProvider
+     * @return void
+     */
+    public function itCanFindMinimalLifetimeForRecordWithStartAndEndTime(array $times, int $expected) {
+        $conn = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('pages');
+
+        $uid = rand(1,9999);
+
+        $conn->insert('pages',['uid' => $uid, 'starttime' => $times['starttime'], 'endtime' => $times['endtime']]);
+
+        $subject = $this->getAccessibleMock(MinimalLifetimeService::class, ['dummy']);
+        $res = $subject->_call('findMinimalLifetimeForRecord', $times['expires'], 'pages', $uid);
+
+        self::assertEquals($expected, $res);
+    }
+
+    public function lifetimeDataProvider(): array {
+
+        $time = time();
+
+        return [
+            'record has endtime and starttime' => [
+                [
+                    'starttime' => $time - 500,
+                    'endtime' => $time + 500,
+                    'expires' => $time + 1000
+                ],
+                $time + 500
+            ],
+            'record has endtime' => [
+                [
+                    'starttime' => 0,
+                    'endtime' => $time + 500,
+                    'expires' => $time + 1000
+                ],
+                $time + 500
+            ],
+            'record does not have time' => [
+                [
+                    'starttime' => 0,
+                    'endtime' => 0,
+                    'expires' => $time + 1000
+                ],
+                $time + 1000
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function itCanFindLifetimeFromAllContentRecordsInPage() {
+        $this->importDataSet('ntf://Database/pages.xml');
+
+        $conn = GeneralUtility::makeInstance(ConnectionPool::class)->getConnectionForTable('tt_content');
+
+        $uid = rand(1,9999);
+        $expires = $GLOBALS['EXEC_TIME'] + 5000;
+        $expectedResult = $GLOBALS['EXEC_TIME'] + 1000;
+
+        // this record has timed out
+        $conn->insert('tt_content', ['pid' => 1, 'uid' => rand(1,999999), 'starttime' => 0, 'endtime' => ($GLOBALS['EXEC_TIME'] - 100)]);
+        // this record is not active yet
+        $conn->insert('tt_content', ['pid' => 1, 'uid' => rand(1,999999), 'starttime' => ($GLOBALS['EXEC_TIME'] + 100), 'endtime' => ($GLOBALS['EXEC_TIME'] + 1000)]);
+        // this is the one we want
+        $conn->insert('tt_content', ['pid' => 1, 'uid' => $uid, 'starttime' => ($GLOBALS['EXEC_TIME'] - 100), 'endtime' => $expectedResult]);
+
+        $subject = $this->getAccessibleMock(MinimalLifetimeService::class, ['getTyposcriptFrontendController']);
+
+        $mockTFSE = $this->getMockBuilder(TypoScriptFrontendController::class)
+        ->disableOriginalConstructor()
+        ->getMock();
+        $mockTFSE->id = 1;
+
+        $subject->expects(self::any())->method('getTyposcriptFrontendController')->willReturn($mockTFSE);
+
+        $res = $subject->_call('findMinimalLifetimeForTable', $expires, 'tt_content', []);
+
+        self::assertEquals($expectedResult, $res);
+    }
+}
---
+++ Tests/Unit/Service/MinimalLifetimeServiceTest.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Netlogix\Nxcachetags\Tests\Unit\Service;
+
+use Netlogix\Nxcachetags\Service\MinimalLifetimeService;
+use Nimut\TestingFramework\MockObject\AccessibleMockObjectInterface;
+use Nimut\TestingFramework\TestCase\UnitTestCase;
+use PHPUnit\Framework\MockObject\MockObject;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
+
+class MinimalLifetimeServiceTest extends UnitTestCase
+{
+    /**
+     * @test
+     * @return void
+     */
+    public function itReturnsLifetimeForRecordIfTableIsNotInTCA() {
+        // this is a unit test, there is no TCA to worry about
+
+        $subject = $this->getAccessibleMock(MinimalLifetimeService::class, ['dummy']);
+        $lifetime = time() + rand(1, 999999);
+
+        $res = $subject->_call('findMinimalLifetimeForRecord', $lifetime, uniqid('table_'), rand(1,999));
+
+        self::assertEquals($lifetime, $res);
+    }
+    /**
+     * @test
+     * @return void
+     */
+    public function itReturnsLifetimeForTableIfTableIsNotInTCA() {
+        // this is a unit test, there is no TCA to worry about
+
+        $subject = $this->getAccessibleMock(MinimalLifetimeService::class, ['dummy']);
+        $lifetime = time() + rand(1, 999999);
+
+        $res = $subject->_call('findMinimalLifetimeForTable', $lifetime, uniqid('table_'), []);
+
+        self::assertEquals($lifetime, $res);
+    }
+
+    /**
+     * @test
+     * @dataProvider storagePidConfigurationDataProvider
+     * @return void
+     */
+    public function itCanGetStoragePidsFromConfig(array $config, array $lifetimeSource, array $expectedResult)
+    {
+        $mockConfigurationManager = $this->getMockBuilder(ConfigurationManager::class)
+            ->disableOriginalConstructor()
+            ->onlyMethods(['getConfiguration'])
+            ->getMock();
+
+        $mockConfigurationManager->method('getConfiguration')->willReturn($config);
+
+        /** @var MockObject|AccessibleMockObjectInterface|MinimalLifetimeService $subject */
+        $subject = $this->getAccessibleMock(MinimalLifetimeService::class, ['dummy']);
+        $subject->injectConfigurationManager($mockConfigurationManager);
+
+        $res = $subject->_call('getStoragePids', $lifetimeSource);
+
+        self::assertEquals($res, $expectedResult);
+    }
+
+    public function storagePidConfigurationDataProvider(): array
+    {
+        return [
+            'empty configuration' => [[], [], []],
+            'table with lifetime' => [['persistence' => ['storagePid' => 999]], [], [999]],
+            'table with lifetime, but is ignored' => [
+                [
+                    'persistence' => [
+                        'storagePid' => 999,
+                        'noStoragePidForCacheLifetime' => ['pages']
+                    ],
+
+                ],
+                ['pages'],
+                []
+            ],
+
+
+        ];
+    }
+
+}
