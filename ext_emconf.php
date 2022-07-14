17ee5c1bf3d6e7fc27ffa38169d16432c280a4ad
Alexander Jahn <alexander.jahn@netlogix.de>
2022-07-14 11:00:39 +0200

    PATCH: update ext_emconf file

--- ext_emconf.php
+++ ext_emconf.php
@@ -19,10 +19,10 @@ $EM_CONF[$_EXTKEY] = [
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '5.0.0-dev',
+    'version' => '5.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
