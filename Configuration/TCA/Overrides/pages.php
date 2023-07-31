e276c387d817d783320f0c6bcdb326a2767969bf
Sascha Nowak <sascha.nowak@netlogix.de>
2023-07-31 19:42:00 +0200

    BUGFIX: Remove undefined TYPO3_MODE constant

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,24 +1,23 @@
 <?php
 declare(strict_types=1);
-defined('TYPO3_MODE') or die();
 
 use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 
 call_user_func(function () {
-    $tempColumns = array(
-        'nxcachetags_cacheperuser' => array(
+    $tempColumns = [
+        'nxcachetags_cacheperuser' => [
             'exclude' => 1,
             'label' => 'LLL:EXT:nxcachetags/Resources/Private/Language/locallang.xlf:pages.nxcachetags_cacheperuser',
-            'config' => array(
+            'config' => [
                 'type' => 'check',
-                'items' => array(
-                    '1' => array(
+                'items' => [
+                    '1' => [
                         '0' => 'LLL:EXT:nxcachetags/Resources/Private/Language/locallang.xlf:pages.nxcachetags_cacheperuser.checkbox_1_formlabel',
-                    )
-                )
-            )
-        ),
-    );
+                    ]
+                ]
+            ]
+        ],
+    ];
 
     ExtensionManagementUtility::addTCAcolumns('pages', $tempColumns);
     ExtensionManagementUtility::addFieldsToPalette('pages', 'caching', 'nxcachetags_cacheperuser');
