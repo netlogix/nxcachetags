9be156710aedb8574a40807fafc2b1ac3458ceff
Moritz Eckert <moritz.eckert@netlogix.de>
2023-09-21 16:51:09 +0200

    test: add GeneratePublicUrlForResourceTest.php

---
+++ Tests/Unit/EventListener/GeneratePublicUrlForResourceTest.php
@@ -0,0 +1,165 @@
+<?php
+declare(strict_types=1);
+
+namespace Netlogix\Nxcachetags\Tests\Unit\EventListener;
+
+use Netlogix\Nxcachetags\EventListener\GeneratePublicUrlForResource;
+use TYPO3\CMS\Core\Resource\Driver\DriverInterface;
+use TYPO3\CMS\Core\Resource\Event\GeneratePublicUrlForResourceEvent;
+use TYPO3\CMS\Core\Resource\File;
+use TYPO3\CMS\Core\Resource\MetaDataAspect;
+use TYPO3\CMS\Core\Resource\ProcessedFile;
+use TYPO3\CMS\Core\Resource\ResourceInterface;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+use TYPO3\TestingFramework\Core\Unit\UnitTestCase;
+
+class GeneratePublicUrlForResourceTest extends UnitTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $GLOBALS['TSFE'] = $this->getMockBuilder(TypoScriptFrontendController::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function itAddCacheTagsWhenInvokedWithFile(): void
+    {
+        $metaDataAspectMock = $this->getMockBuilder(MetaDataAspect::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $metaDataAspectMock->method('get')
+            ->willReturn([
+                'uid' => '41'
+            ]);
+
+        $fileMock = $this->getMockBuilder(File::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $fileMock->method('getMetaData')
+            ->willReturn($metaDataAspectMock);
+        $fileMock->method('getUid')
+            ->willReturn(42);
+
+
+
+        $GLOBALS['TSFE']
+            ->expects($this->once())
+            ->method('addCacheTags')
+            ->with(['sys_file_42', 'sys_file_metadata_41']);
+
+        $event = new GeneratePublicUrlForResourceEvent(
+            $fileMock,
+            $this->createMock(ResourceInterface::class),
+            $this->createMock(DriverInterface::class)
+        );
+
+        $subject = new GeneratePublicUrlForResource();
+        $subject->__invoke($event);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function itGetsOriginalFileOfProcessedFile(): void
+    {
+        $fileMock = $this->getMockBuilder(File::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $processedFileMock = $this->getMockBuilder(ProcessedFile::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $processedFileMock->expects($this->once())
+            ->method('getOriginalFile')
+            ->willReturn($fileMock);
+
+        $event = new GeneratePublicUrlForResourceEvent(
+            $processedFileMock,
+            $this->createMock(ResourceInterface::class),
+            $this->createMock(DriverInterface::class)
+        );
+
+        $subject = new GeneratePublicUrlForResource();
+        $subject->__invoke($event);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function itDoesNotAddCacheTagsIfTSFCIsNull(): void
+    {
+        $fileMock = $this->getMockBuilder(File::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $GLOBALS['TSFE'] = null;
+
+        $event = new GeneratePublicUrlForResourceEvent(
+            $fileMock,
+            $this->createMock(ResourceInterface::class),
+            $this->createMock(DriverInterface::class)
+        );
+
+        $subject = new GeneratePublicUrlForResource();
+        $subject->__invoke($event);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function itDoesNotAddCacheTagsMoreThenOnce(): void
+    {
+        $fileMock = $this->getMockBuilder(File::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+        $fileMock->method('getUid')
+            ->willReturn(42);
+
+        $GLOBALS['TSFE']
+            ->expects($this->never())
+            ->method('addCacheTags');
+
+        $GLOBALS['TSFE']
+            ->method('getPageCacheTags')
+            ->willReturn(['sys_file_42']);
+
+        $event = new GeneratePublicUrlForResourceEvent(
+            $fileMock,
+            $this->createMock(ResourceInterface::class),
+            $this->createMock(DriverInterface::class)
+        );
+
+        $subject = new GeneratePublicUrlForResource();
+        $subject->__invoke($event);
+    }
+
+    /**
+     * @test
+     * @return void
+     */
+    public function itDoesNotAddCacheTagsIfFileIsNotInstanceOfFileInterface (): void
+    {
+        $event = new GeneratePublicUrlForResourceEvent(
+            $this->createMock(ResourceInterface::class),
+            $this->createMock(ResourceInterface::class),
+            $this->createMock(DriverInterface::class)
+        );
+
+        $GLOBALS['TSFE']
+            ->expects($this->never())
+            ->method('addCacheTags');
+
+        $subject = new GeneratePublicUrlForResource();
+        $subject->__invoke($event);
+    }
+}
+
+
